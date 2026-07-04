be8dae11c574923c5a5b419fbbe9be4e67d16e97
Harpreet <iamharpreet73@gmail.com>
2026-07-04 13:14:25 +0530

    refactor: update landing layout and sidebar components with routing and styling improvements

--- frontend/src/app/layouts/landing-layout/landing-layout.ts
+++ frontend/src/app/layouts/landing-layout/landing-layout.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../../shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-landing-layout',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet, Navbar],
   templateUrl: './landing-layout.html',
   styleUrl: './landing-layout.css',
 })
--- frontend/src/app/pages/landing/landing.ts
+++ frontend/src/app/pages/landing/landing.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../shared/components/navbar/navbar';
+import { RouterLink, RouterLinkActive } from '@angular/router'
 
 @Component({
   selector: 'app-landing',
-  imports: [Navbar],
+  imports: [Navbar, RouterLink, RouterLinkActive],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
-export class Landing {
+export class Landing{
   isMenuOpen = false;
 
   toggleMenu() {
